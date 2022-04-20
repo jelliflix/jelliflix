fa61ccb2a6393bb5d70cf1ecdaf3e3b5ad6edcca
Sadegh Ramezani <me@ramezanius.me>
2022-04-20 23:15:37 +0430

    Implement and update store structures

--- store/store.go
+++ store/store.go
@@ -1,9 +1,19 @@
 package store
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/go-redis/redis/v8"
 	"github.com/jelliflix/jelliflix/infrastructure/datastore"
 )
+
+var (
+	Movies   Kind = "M"
+	Episodes Kind = "EP"
+)
+
+type Kind string
 
 type Store struct {
 	redis *redis.Client
@@ -14,34 +24,48 @@ func NewStore() *Store {
 	return &Store{redis: r}
 }
 
-func (s *Store) AddMovie(imdbID string) (exists bool) {
+func (s *Store) Set(ctx context.Context, kind Kind, imdbID, filename string) {
+	s.redis.Set(ctx, s.getKey(kind, imdbID), filename, 0)
+}
+
+func (s *Store) Get(ctx context.Context, kind Kind) (items map[string]string) {
+	keys := s.redis.Keys(ctx, s.getKey(kind, "*")).Val()
+
+	for _, key := range keys {
+		filename := s.redis.Get(ctx, key).Val()
+		items[key] = filename
+	}
+
+	return
+}
+
+func (s *Store) Purge(ctx context.Context, kind Kind, items []string) {
 	panic("implement me")
 }
 
-func (s *Store) SaveMovie(imdbID, filename string) {
-	panic("implement me")
+func (s *Store) Remove(ctx context.Context, kind Kind, imdbID string) {
+	s.redis.Del(ctx, s.getKey(kind, imdbID))
 }
 
-func (s *Store) RemoveMovie(imdbID string) {
-	panic("implement me")
+func (s *Store) Exists(ctx context.Context, kind Kind, imdbID string) bool {
+	return s.redis.Get(ctx, s.getKey(kind, imdbID)).Val() != ""
 }
 
-func (s *Store) AddEpisode(imdbID string) (exists bool) {
-	panic("implement me")
+func (s *Store) getKey(kind Kind, imdbID string) string {
+	switch kind {
+	case Movies:
+		return s.moviesKey(imdbID)
+	case Episodes:
+		return s.episodesKey(imdbID)
+	}
+
+	return ""
 }
 
-func (s *Store) SaveEpisode(imdbID, filename string) {
-	panic("implement me")
+func (s *Store) moviesKey(imdbID string) string {
+	return fmt.Sprintf("m:%s", imdbID)
 }
 
-func (s *Store) RemoveEpisode(imdbID string) {
-	panic("implement me")
-}
-
-func (s *Store) PurgeMovies(movies []string) {
-	panic("implement me")
-}
-
-func (s *Store) PurgeSeries(episodes []string) {
-	panic("implement me")
+func (s *Store) episodesKey(imdbID string) string {
+	return fmt.Sprintf("ep:%s", imdbID)
 }
